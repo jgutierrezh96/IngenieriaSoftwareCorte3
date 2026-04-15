59b1217d6b1db3ca1744406508066efd6269f806
Kurexg <jgutierrezh@unincca.edu.co>
2026-04-14 21:23:51 -0500

    Configure GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const basePath = isGitHubPages && repoName ? `/${repoName}` : '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: basePath
+		},
+		prerender: {
+			handleHttpError: 'warn'
+		}
 	}
 };
 
